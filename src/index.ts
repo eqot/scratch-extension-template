f9241fae3213309ec08e7acf8dda45b976775db6
Ikuo Terado <eqobar@gmail.com>
2020-11-09 00:29:16 +0900

    Build extension for preset

--- src/index.ts
+++ src/index.ts
@@ -2,32 +2,59 @@ import { BLOCKS_ORDER } from './blocks/order';
 import { generateBlocks } from './utils/blocks';
 import { translations } from './utils/translations';
 
-class DummyExtension {
+class Extension {
+  private static ID = 'dummy';
+
   private runtime: unknown;
   private blocks;
 
   constructor(runtime: unknown, locale?: string) {
     this.runtime = runtime;
 
-    translations.initialize(this.runtime, locale);
+    translations.initialize(locale, this.runtime);
 
     this.blocks = generateBlocks(BLOCKS_ORDER, this);
   }
 
+  // Information for scratch-vm
   getInfo(): unknown {
     return {
-      id: 'dummy',
-      name: 'Dummy',
-      menuIconURI: require('../assets/images/menuIcon.png'),
+      id: Extension.ID,
+      name: translations.label('extension.name'),
       blockIconURI: require('../assets/images/blockIcon.png'),
+      menuIconURI: require('../assets/images/menuIcon.png'),
       color1: '#a0a0a0',
       color2: '#808080',
       color3: '#606060',
-
       blocks: this.blocks.info(),
       menus: this.blocks.menus(),
+      // showStatusButton: false,
+    };
+  }
+
+  // Information for scratch-gui
+  static getInfoForGui(): unknown {
+    translations.initialize();
+
+    return {
+      name: translations.label('extension.name'),
+      extensionId: Extension.ID,
+      collaborator: translations.label('extension.collaborator'),
+      iconURL: require('../assets/images/icon.png'),
+      insetIconURL: require('../assets/images/blockIcon.png'),
+      description: translations.label('extension.description'),
+      featured: true,
+      // disabled: false,
+      // bluetoothRequired: false,
+      // internetConnectionRequired: false,
+      // launchPeripheralConnectionFlow: false,
+      // useAutoScan: false,
+      // connectionIconURL: null,
+      // connectionSmallIconURL: null,
+      // connectingMessage: 'Connecting...',
+      // helpLink: 'https://github.com/eqot/scratch-extension-template/',
     };
   }
 }
 
-export default DummyExtension;
+export default Extension;
